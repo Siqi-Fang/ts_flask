1b962823bdcd96b85c09336ea6be2caa04a4bec7
Siqi Fang <sfang@hamilton.edu>
2022-04-04 23:51:33 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from memory_profiler import profile
-
-STOPWORDS = []
-with open('eng_stopword.txt', 'r') as f:
-    for word in f:
-        word = word.split()[0]
-        STOPWORDS.append(word)
-print(STOPWORDS)
-#if __name__ == '__main__':
